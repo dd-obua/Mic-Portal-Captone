705a65f26f5b9de623351995ca2c0ce206375c2b
Obua Denis Daniel <dd.obua@gmail.com>
2023-08-02 21:29:42 +0300

    Update picture object

--- js/script.js
+++ js/script.js
@@ -15,35 +15,50 @@ closeMenuIcon.addEventListener('click', closeMenu);
 
 const experts = [
   {
-    picture: './images/experts/expert-1.jpeg',
+    picture: {
+      src: './images/experts/expert-1.jpeg',
+      alt: 'Picture of the first expert',
+    },
     fullName: 'Dr. Jennifer Adam',
     expertise:
       'Artificial Intelligence and Robotics. Her research focuses on developing AI-driven robots for healthcare applications',
     workPlace: 'Massachusetts Institute of Technology (MIT) - Robotics Lab',
   },
   {
-    picture: './images/experts/expert-2.jpeg',
+    picture: {
+      src: './images/experts/expert-2.jpeg',
+      alt: 'Picture of the second expert',
+    },
     fullName: 'Prof. Mark Johnson',
     expertise:
       'Cybersecurity and Ethical Hacking. He is an authority on cybersecurity strategies and has conducted numerous penetration testing projects',
     workPlace: '',
   },
   {
-    picture: './images/experts/expert-3.jpeg',
+    picture: {
+      src: './images/experts/expert-3.jpeg',
+      alt: 'Picture of the third expert',
+    },
     fullName: 'Dr. Emily Carter',
     expertise:
       'Quantum Computing and Computational Chemistry. Her pioneering work bridges the gap between quantum mechanics and chemistry',
     workPlace: 'Princeton University - Department of Chemistry',
   },
   {
-    picture: './images/experts/expert-4.jpeg',
+    picture: {
+      src: './images/experts/expert-4.jpeg',
+      alt: 'Picture of the fourth expert',
+    },
     fullName: 'John Lee',
     expertise:
       'Berkman Professor of Entrepreneurial Legal Studies at Harvard Law School',
     workPlace: 'Apple Inc. - Augmented Reality Research Team',
   },
   {
-    picture: './images/experts/expert-5.jpeg',
+    picture: {
+      src: './images/experts/expert-5.jpeg',
+      alt: 'Picture of the fifth expert',
+    },
     fullName: 'Dr. Alex Martinez',
     expertise:
       'Internet of Things (IoT) and Smart Cities. His research focuses on implementing IoT technologies to enhance urban sustainability',
@@ -51,7 +66,10 @@ const experts = [
       'University of California, Berkeley - Institute for Smart Cities',
   },
   {
-    picture: './images/experts/expert-6.jpeg',
+    picture: {
+      src: './images/experts/expert-6.jpeg',
+      alt: 'Picture of the sixth expert',
+    },
     fullName: 'Sarah Williams',
     expertise:
       'Blockchain and Cryptocurrencies. She is a renowned blockchain expert, working on secure  and calable blockchain solutions',
